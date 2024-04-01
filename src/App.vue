01282ceb324ad5d9801709e52a2aec88a4359c33
CcccFz <ccccfz@163.com>
2024-04-01 23:44:49 +0800

    update: study to v-for

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,46 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, computed, onMounted, onUpdated } from 'vue'
+
+import TodoList from './TodoList.vue'
+
+const cnt = ref(0)
+const ifFlag = ref(true)
+
+function incr() {
+  cnt.value++
+}
+
+function reduce() {
+  cnt.value--
+}
+
+const isPositive = computed(() => {
+  return cnt.value >= 0 ? '正数' : '负数'
+})
+
+function isPositive1() {
+  return cnt.value >= 0 ? '正数' : '负数'
+}
+
+onMounted(() => {
+  console.log('The init count is: 0')
+})
+
+onUpdated(() => {
+  console.log(`The init count is: ${cnt.value}`)
+})
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <div>The count is: {{ cnt }}, is {{ isPositive1() }}</div>
+  <div>The count is: {{ cnt }}, is {{ isPositive }}</div>
+  <button @click="incr">+</button>
+  <button @click="reduce">-</button>
+  <div>
+    <div v-if="ifFlag">Vue is good !</div>
+    <div v-else>Oh no !!!</div>
+    <button @click="ifFlag = !ifFlag">Toggle</button>
+  </div>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <TodoList></TodoList>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
---
+++ src/TodoList.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const inputTitle = ref('')
+const todos = ref([])
+let id = 0
+
+function addTodo() {
+  todos.value.push({ title: inputTitle.value, id: ++id })
+  inputTitle.value = ''
+}
+
+function removeTodo(idx) {
+  todos.value.splice(idx, 1)
+  id--
+}
+</script>
+
+<template>
+  <div>
+    <form @submit.prevent="addTodo">
+      <label for="add-todo"></label>
+      <input id="add-todo" v-model="inputTitle" />
+      <button>New</button>
+    </form>
+    <ul>
+      <li v-for="(todo, idx) of todos" :key="todo.id">
+        {{ todo.title }}
+        <button @click="removeTodo(idx)">-</button>
+      </li>
+    </ul>
+  </div>
+</template>
